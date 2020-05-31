01bc3d2b777acea5f7e6028b6f25e2eb672f67f6
Andrey <Sibir_91@list.ru>
2020-05-31 20:07:52 +0400

    animation add

--- src/js/script.js
+++ src/js/script.js
@@ -53,7 +53,7 @@ $(document).ready(function(){
         })
     });
 
-    function validateForms(form) {
+    function validateForms(form){
         $(form).validate({
             rules: {
                 name: "required",
@@ -79,4 +79,38 @@ $(document).ready(function(){
     validateForms('#order form');
 
     $('input[name="phone"]').mask("+7 (999) 999-99-99")
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //scroll
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1400) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
   });
